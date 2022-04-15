d32ff80b39901f38e6eafb316462d0f00e854baf
Rogerio Pereira <hi@rogeriopereira.dev>
2022-04-15 18:19:04 -0400

    Routes configuration (remove web)

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return response()->json(['message' => 'ok']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
